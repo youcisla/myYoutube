ddd7533d07fb174ec166feb1ce1ad8e77d1f59cd
chehbo_y <chehbo_y@etna-alternance.net>
2024-12-05 11:56:38 +0100

    Create videos

--- Project/myYoutube/MyAPI/Django/settings.py
+++ Project/myYoutube/MyAPI/Django/settings.py
@@ -145,9 +145,11 @@ AUTH_USER_MODEL = 'MyAPI.MyUser'
 CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:4200',
+    'http://localhost:3000',
 ]
 
 
 APPEND_SLASH = False
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
